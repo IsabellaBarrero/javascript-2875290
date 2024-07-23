b4c191f37ce83e0722f5c163768725e37404e8c5
IsabellaBarrero <isabella.barrero.93@gmail.com>
2024-07-23 15:21:44 -0500

    lista tareas

--- 03-dinamic-dom/app.js
+++ 03-dinamic-dom/app.js
@@ -4,30 +4,37 @@ const main = document.querySelector('#main');
 const btnBorrar = document.querySelector('#btnBorrar');
 let n = 1;
 
-
 function crear (){
+    
     const ball = document.createElement('div');
+    const equis = document.createElement('button');
+
+    equis.innerText = 'x';
+    equis.classList.add('equis');
+
     ball.classList.add('pelotica');
-    ball.style.backgroundColor = listaColores.value
+    ball.style.backgroundColor = listaColores.value;
     ball.innerText = n++;
+/*     ball.innerHTML = equis; */
+    ball.appendChild(equis)
 
     main.appendChild(ball);
-
-    console.log(ball)
 }
-
 
 function borrarPantalla (){
-    main.innerHTML ='';
-    n = 1
+    main.innerHTML = '';
+    n=1;
+}
+function borrarElemento (e){ 
+    if(e.target.innerText == 'x'){
+        main.removeChild(e.target.parentElement);
+    }
+}
+function completarTarea (e){ 
+    e.target.style.textDecoration = 'line-through';
 }
 
-function borrarElemento (e){
-    console.log('le di click a la pelotica: ' + e.target.innerText);
-    main.removeChild(e.target);
-    
-}
-
-btnCrear.addEventListener('click', crear)
-btnBorrar.addEventListener('click', borrarPantalla)
-main.addEventListener('click', borrarElemento)
+btnCrear.addEventListener('click',crear)
+btnBorrar.addEventListener('click',borrarPantalla)
+main.addEventListener('click',borrarElemento)
+main.addEventListener('click',completarTarea)
