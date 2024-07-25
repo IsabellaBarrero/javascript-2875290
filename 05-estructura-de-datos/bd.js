fb3461766cfcc893a2e13dbe32b1cfa56d2d0195
IsabellaBarrero <isabella.barrero.93@gmail.com>
2024-07-25 15:55:11 -0500

    avance

---
+++ 05-estructura-de-datos/bd.js
@@ -0,0 +1,91 @@
+let musica = [
+    {
+        id: 1,
+        nombre: 'Jump Dance Move Bounce',
+        artista: ['AENJAY', 'Nicolas Julian', 'NOTMYTYPE'],
+        año: 2023,
+        genero: ['Neo Rave', 'Hardcore', 'Hard Dance'],
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/bb49544d-5521-4abc-8a8b-1ab19dcfbcf7.jpg',
+    },
+
+    {
+        id: 2,
+        nombre: 'industrialyzer',
+        artista: 'Project Mantis',
+        año: 2020,
+        genero: ['Techno', 'Deep', 'Hypnotic'],
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/46d62e87-a04b-405b-8f62-1f7769e138eb.jpg',
+    },
+
+    {
+        id: 3,
+        nombre: 'cl9',
+        artista: 'Lady Starlight',
+        año: 2020,
+        genero: 'techno',
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/fa2555bc-297b-45c5-b1bb-bf9517613609.jpg',
+    },
+
+    {
+        id: 4,
+        nombre: 'Dont Get Lost in Heaven',
+        artista: 'BIIA',
+        año: 2023,
+        genero: ['Neo Rave', 'Hardcore', 'Hard Dance'],
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/7cc4cca8-dbcf-4026-bf65-8e5ac8ea93cd.jpg',
+    },
+
+    {
+        id: 5,
+        nombre: 'Hear The Sound',
+        artista: 'Franck',
+        año: 2022,
+        genero: 'Hard Techno',
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/c89538ed-e0f3-4f60-922e-b2e170064317.jpg',
+    },
+
+    {
+        id: 6,
+        nombre: 'Satan Was A Babyboomer',
+        artista: 'Brutalismus 3000',
+        año: 2021,
+        genero: ['Neo Rave', 'Hardcore', 'Hard Dance'],
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/bcd8dbd3-f3d7-45e4-88ff-5d03940ff626.jpg',
+    },
+
+    {
+        id: 7,
+        nombre: 'Romantika',
+        artista: 'Brutalismus 3000',
+        año: 2022,
+        genero: ['Neo Rave', 'Hardcore', 'Hard Dance'],
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/db68f378-8101-4da2-89a2-2fe287f47db4.jpg',
+    },
+
+    {
+        id: 8,
+        nombre: 'Cola',
+        artista: [' CamelPhat', 'Elderbrook'],
+        año: 2017,
+        genero:  'House',
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/2f5e5d49-75c3-47c2-9adb-c2dad46ab5f2.jpg',
+    },
+
+    {
+        id: 9,
+        nombre: 'Free',
+        artista: 'Ultra Nate',
+        año: 1997,
+        genero:  ['House', 'Dance', 'Electro Pop'],
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/e26e9581-a77c-4351-8388-38c9077e36e3.jpg',
+    },
+
+    {
+        id: 10,
+        nombre: 'Innerbloom',
+        artista: 'RÜFÜS DU SOL',
+        año: 2016,
+        genero:  'Tech House',
+        imagen: 'https://geo-media.beatport.com/image_size/1400x1400/31d92299-45c1-42af-ab8b-a3d0f4016d97.jpg',
+    },
+]
