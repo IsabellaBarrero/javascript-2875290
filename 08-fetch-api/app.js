9f53097a5dd4a6f3e5c8d041b3ed5bc2a98529fa
IsabellaBarrero <isabella.barrero.93@gmail.com>
2024-08-14 15:51:32 -0500

    fetch

--- 08-fetch-api/app.js
+++ 08-fetch-api/app.js
@@ -1,33 +1,19 @@
+const container = document.querySelector(".container")
 
- const container = document.querySelector('.container')
-
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jsonplaceholder.typicode.com/users/')
   .then(respuesta => respuesta.json())
   .then(datos => {
     datos.forEach(element => {
-        //name:
-        //usernamee:
-        console.log(`name: ${element.name}`)
-        console.log(`username: ${element.username}`)
+        let box = document.createElement('div')
+        box.classList.add("box");
+        container.appendChild(box)
 
-       
-        const box = document.createElement('div');
-        box.classList.add('box');
+        const title = document.createElement("h1")
+        title.innerText = `Name: ${element.name}`
+        box.appendChild(title)
 
-        const h1 = document.createElement('h1');
-        h1.textContent = element.name;
-        box.appendChild(h1);
-
-        const p = document.createElement('p');
-        p.textContent = element.username;
-        box.appendChild(p);
-
-        
-        
-        container.appendChild(box);
-
-
-
-    
+        const text = document.createElement("p")
+        text.innerText = `Userame: ${element.username}`
+        box.appendChild(text)
     });
   })
